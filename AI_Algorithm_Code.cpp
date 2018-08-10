72db51c15798cec7e511559b73eedeeef712f780
Xig <xcv6018@naver.com>
2018-08-10 09:03:47 +0900

    Add files via upload

--- AI_Algorithm_Code.cpp
+++ AI_Algorithm_Code.cpp
@@ -2,22 +2,34 @@
 
 /*
 [AI 코드 작성 방법]
+
 1. char info[]의 배열 안에					"TeamName:자신의 팀명,Department:자신의 소속"					순서로 작성합니다.
 ( 주의 ) Teamname:과 Department:는 꼭 들어가야 합니다.
 "자신의 팀명", "자신의 소속"을 수정해야 합니다.
+
 2. 아래의 myturn() 함수 안에 자신만의 AI 코드를 작성합니다.
+
 3. AI 파일을 테스트 하실 때는 "육목 알고리즘대회 툴"을 사용합니다.
+
 4. 육목 알고리즘 대회 툴의 연습하기에서 바둑돌을 누른 후, 자신의 "팀명" 이 들어간 알고리즘을 추가하여 테스트 합니다.
+
+
+
 [변수 및 함수]
 myturn(int cnt) : 자신의 AI 코드를 작성하는 메인 함수 입니다.
 int cnt (myturn()함수의 파라미터) : 돌을 몇 수 둬야하는지 정하는 변수, cnt가 1이면 육목 시작 시  한 번만  두는 상황(한 번), cnt가 2이면 그 이후 돌을 두는 상황(두 번)
 int  x[0], y[0] : 자신이 둘 첫 번 째 돌의 x좌표 , y좌표가 저장되어야 합니다.
 int  x[1], y[1] : 자신이 둘 두 번 째 돌의 x좌표 , y좌표가 저장되어야 합니다.
 void domymove(int x[], int y[], cnt) : 둘 돌들의 좌표를 저장해서 출력
+
+
 //int board[BOARD_SIZE][BOARD_SIZE]; 바둑판 현재상황 담고 있어 바로사용 가능함. 단, 원본데이터로 수정 절대금지
 // 놓을수 없는 위치에 바둑돌을 놓으면 실격패 처리.
+
 boolean ifFree(int x, int y) : 현재 [x,y]좌표에 바둑돌이 있는지 확인하는 함수 (없으면 true, 있으면 false)
 int showBoard(int x, int y) : [x, y] 좌표에 무슨 돌이 존재하는지 보여주는 함수 (1 = 자신의 돌, 2 = 상대의 돌, 3 = 블럭킹)
+
+
 <-------AI를 작성하실 때, 같은 이름의 함수 및 변수 사용을 권장하지 않습니다----->
 */
 
@@ -26,357 +38,263 @@ int showBoard(int x, int y) : [x, y] 좌표에 무슨 돌이 존재하는지 보여주는 함수 (
 #include <time.h>
 #include "Connect6Algo.h"
 
-#include <cmath>
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 // "샘플코드[C]"  -> 자신의 팀명 (수정)
 // "AI부서[C]"  -> 자신의 소속 (수정)
 // 제출시 실행파일은 반드시 팀명으로 제출!
 char info[] = { "TeamName:1234567890,Department:AI부서[C]" };
 
-float inclination;			// 바둑돌 선형성 검사 변수
-int val[19][19];			// 전체 가중치를 나타내는 배열
-int center_x, center_y;		// 중심이 되는 x, y 좌표 값
-int tracker[19][19];
-std::vector<int> mustDo;
-std::vector<Position> promPos;
+using namespace std;
+
+vector<Position> myLog;
+vector<Position> enemyLog;
+double inclination;
+int virtualBoard[19][19];
 
 void myturn(int cnt) {
 
 	int x[2], y[2];
-	int maxVal = 1;			// 가중치 중 가장 큰 값
-
-							// 입출력으로 log 확인
-	FILE *file_pointer;
-	file_pointer = fopen("C:\\Users\\노유찬\\Desktop\\육목\\삼성전자DS부문육목SW알고리즘대회\\Log\\log.txt", "a");
 
 	// 이 부분에서 알고리즘 프로그램(AI)을 작성하십시오. 기본 제공된 코드를 수정 또는 삭제하고 본인이 코드를 사용하시면 됩니다.
 	// 현재 Sample code의 AI는 Random으로 돌을 놓는 Algorithm이 작성되어 있습니다.
 
 	srand((unsigned)time(NULL));
 
-	// 가중치 0으로 초기화
-	for (int i = 0; i < width; i++) {
-		for (int j = 0; j < height; j++) {
-			val[i][j] = 0;
-		}
-	}
+	FILE *file_pointer;
+	file_pointer = fopen("C:\\Users\\노유찬\\Desktop\\육목\\Version\\2018_08_09\\log.txt", "a");
 
-	if (empty()) {			// 첫 수 둘 경우
-		x[0] = 9;			// 9, 9 를 기본
-		y[0] = 9;
+	if (cnt == 1) {
+		x[0] = 9; y[0] = 9;
 
-		while (!isFree(x[0], y[0])) {		// 돌이 다른 것이 있을 경우(block)
-			int sign = rand() % 2;			// 부호를 음수로 할 지, 양수로 할 지
+		while (!isFree(x[0], y[0])) {		
+			int sign = rand() % 2;			
 
-			if (sign) {						// 양수일 경우 랜덤하게 좌표를 더함
+			if (sign == 1) {						
 				x[0] += rand() % 2;
 				y[0] += rand() % 2;
 			}
-			else {							// 음수일 경우 랜덤하게 좌표를 뺌
+			else {							
 				x[0] -= rand() % 2;
 				y[0] -= rand() % 2;
 			}
 		}
 	}
 	else {
-		check();
+		for(int i=0; i<cnt; i++) {
+			Node *solution = yukmok();
+			x[i] = solution->getX();
+			y[i] = solution->getY();
+		}
 	}
 
-	// 로그 출력
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
-			fprintf(file_pointer, "%d ", val[i][j]);
+			fprintf(file_pointer, "%d ", virtualBoard[j][i]);
 		}
 		fprintf(file_pointer, "\n");
 	}
 	fprintf(file_pointer, "\n\n");
 	
-	for (int k = 0; k < cnt; k++) {
-		if (mustDo.size() > 0) {
-			x[k] = mustDo.back();
-			mustDo.pop_back();
-			y[k] = mustDo.back();
-			mustDo.pop_back();
-		}
-		else {
-			maxVal = 1;
-			for (int j = 0; j < height; j++) {
-				for (int i = 0; i < width; i++) {
-					if (maxVal < val[j][i]) {
-						maxVal = val[j][i];
-
-						x[k] = i;
-						y[k] = j;
-					}
-				}
-			}
-		}
-
-		val[y[k]][x[k]] = -1;
-
-		if (terminateAI) return;
-
-	}
-
 	// 이 부분에서 자신이 놓을 돌을 출력하십시오.
 	// 필수 함수 : domymove(x배열,y배열,배열크기)
 	// 여기서 배열크기(cnt)는 myturn()의 파라미터 cnt를 그대로 넣어야합니다.
 	domymove(x, y, cnt);
-
 }
 
-void check() {
-	// 우선순위 1 : 내 돌 중 6개를 만들 수 있는 곳이 있는지 확인
-	checkMyWin();
-	// 우선순위 2 : 적 돌 중 6개를 만들 수 있는 곳이 있는지 확인
-	checkEnemyWin();
-	// 우선순위 3 : 내돌과 적돌을 봐가면서 가중치에 따른 내 돌 놓기
-	setMyWeight();
-	setEnemyWeight();
-}
-
-void checkMyWin() {
-	if(myLog.size() > 0) {
-		for (Position &p : myLog) {
-			int curX = p.getX();
-			int curY = p.getY();
-		
-			val[curY][curX] = -1;
-
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
-					tracker[i][j] = 0;				// 초기화
-				}
-			}
-
-			checkMyPromising(curX, curY, 1);
-		}
-	}
-}
-
-void checkMyPromising(int curX, int curY, int level) {
-	if (curX - 1 >= 0 && curX + 1 <= 19 && curY - 1 >= 0 && curY + 1 <= 19) {
-		for (int x = curX - 1; x <= curX + 1; x++) {
-			for (int y = curY - 1; y <= curY + 1; y++) {
-				int pos = showBoard(x, y);
-				bool line = lineCheck(curX, curY, x, y, level);
-
-				if ((x == curX && y == curY) || tracker[x][y] == 1) continue;
-
-				// 본격적인 검사
-				if (line) { 
-					if (level == 6) {
-						for (Position &p : promPos) {
-							mustDo.push_back(p.getX());
-							mustDo.push_back(p.getY());
-						}
-					}
-					else {
-						if (pos == 0 && promPos.size() < 2) {
-							promPos.push_back(Position(x, y));
-							checkMyPromising(x, y, level + 1);
-						}
-						else if (pos == 1 || pos == 3) {
-							checkMyPromising(x, y, level + 1);
-						}
-						else {
-							promPos.clear();
-						}
-					}
-				}
-			}
-		}
-	}
-	else {
-		return;
-	}
-}
-
-void checkEnemyWin() {
-	if (enemyLog.size() > 0) {
-		for (Position &p : enemyLog) {
-			int curX = p.getX();
-			int curY = p.getY();
-
-			val[curY][curX] = -1;
-
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
-					tracker[i][j] = 0;				// 초기화
-				}
-			}
-
-			checkEnemyPromising(curX, curY, 1);
-		}
-	}
-}
-
-void checkEnemyPromising(int curX, int curY, int level) {
-	if (curX - 1 >= 0 && curX + 1 <= 19 && curY - 1 >= 0 && curY + 1 <= 19) {
-		for (int x = curX - 1; x <= curX + 1; x++) {
-			for (int y = curY - 1; y <= curY + 1; y++) {
-				int pos = showBoard(x, y);
-				bool line = lineCheck(curX, curY, x, y, level);
-
-				if ((x == curX && y == curY) || tracker[x][y] == 1) continue;
-
-				// 본격적인 검사
-				if (line) {
-					if (level == 6) {
-						for (Position &p : promPos) {
-							mustDo.push_back(p.getX());
-							mustDo.push_back(p.getY());
-						}
-					}
-					else {
-						if (pos == 0 && promPos.size() < 2) {
-							promPos.push_back(Position(x, y));
-							checkEnemyPromising(x, y, level + 1);
-						}
-						else if (pos == 2 || pos == 3) {
-							checkEnemyPromising(x, y, level + 1);
-						}
-						else {
-							promPos.clear();
-						}
-					}
-				}
-			}
-		}
-	}
-	else {
-		return;
-	}
-}
-
-// 내 돌 검사
-void setMyWeight() {
-	if (myLog.size() > 0) {
-		for (Position &p : myLog) {
-			int x = p.getX();
-			int y = p.getY();
-			center_x = x;
-			center_y = y;
-
-			val[y][x] = -1;
-
-			// 검사했던 곳을 다시 검사하지 않도록 myTracker 사용
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
-					tracker[i][j] = 0;				// 초기화
-				}
-			}
-
-			searchMyTree(x, y, 1);						// level 1부터 시작
-		}
-	}
-}
-
-void searchMyTree(int x, int y, int level) {
-	// 3x3 검사
-	for (int j = y - 1; j <= y + 1; j++) {
-		for (int i = x - 1; i <= x + 1; i++) {
-
-			int pos = showBoard(i, j);
-			bool line = lineCheck(x, y, i, j, level + 1);
-
-			if (i == x && j == y) continue;				// 전에 뒀던 x, y 좌표 pass
-			else if (tracker[i][j] == 1) continue;	// tracker를 이용해서 전에 검사했던 부분을 pass
-
-			if (pos == 0) {
-				val[j][i] += 50;
-				if (line == true) val[j][i] += 200 * level;
-			}
-			else if (pos == 1 || pos == 3) {
-				val[j][i] = -1;
-				tracker[i][j] = 1;
-				if (line == true) searchMyTree(i, j, level + 1);
-				else searchMyTree(i, j, 1);
-			}
-			else {
-				if (line == true) {
-					val[j][i] -= 20;
-					set_opposition(i, j);
-				}
-				else val[j][i] -= 10;
-			}
-		}
-	}
-}
-
-
-// 적 돌 검사
-void setEnemyWeight() {
-	if (enemyLog.size() > 0) {
-		for (Position &p : enemyLog) {
-			int x = p.getX();
-			int y = p.getY();
-
-			val[y][x] = -1;
-
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
-					tracker[i][j] = 0;
-				}
-			}
-
-			searchEnemyTree(x, y, 1);	// level 1부터 시작
-		}
-	}
-}
-
-void searchEnemyTree(int x, int y, int level) {
-
-	for (int j = y - 1; j <= y + 1; j++) {
-		for (int i = x - 1; i <= x + 1; i++) {
-			int pos = showBoard(i, j);
-			bool line = lineCheck(x, y, i, j, level + 1);
-
-			if (i == x && j == y) continue;
-			else if (tracker[i][j] == 1) continue;
-			else tracker[i][j] = 1;
-
-			if (pos == 0) {
-				val[j][i] += 50;
-				if (line == true) val[j][i] += 200 * level;
-			}
-			else if (pos == 2 || pos == 3) {
-				val[j][i] = -1;
-				tracker[i][j] = 1;
-				if (line == true) searchEnemyTree(i, j, level + 1);
-				else searchEnemyTree(i, j, 1);
-			}
-			else {
-				if (line == true) {
-					val[j][i] -= 20;
-					set_opposition(i, j);
-				}
-				else val[j][i] -= 10;
-			}
-		}
-	}
-}
-
-// 부가적인 함수들
-
-// 보드가 빈 상태인지 확인
-bool empty() {
+Node* yukmok() {
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
-			if (showBoard(i, j) == 1 || showBoard(i, j) == 2) return false;
+			virtualBoard[i][j] = showBoard(i, j);
+		}
+	}
+	Position &p = myLog.back();
+	myLog.pop_back();
+	Node *root = new Node(p.getX(), p.getY());
+	root->setUnit(showBoard(p.getX(), p.getY()));
+	virtualBoard[p.getX()][p.getY()] = showBoard(p.getX(), p.getY());
+	return MCTS(root);
+}
+
+Node* MCTS(Node* root) {
+	int turn = -1, count = 2;
+	if (root->getUnit() == 1) {
+		turn = 1; count = 1;
+	}
+	Node *child = new Node();
+	for (int i = 0; i < 100; i++) {
+		child = selection(root, turn);
+		int simulation_result = simulation(child);
+		backpropagate(child, simulation_result);
+	}
+
+	return best_child(root->child);
+}
+
+Node* selection(Node* node, int turn) {
+	// 자식 노드를 최대한 만든다.
+	while (non_terminal(node)) {
+		if (!fully_expanded(node)) {
+			node = expand(node, turn);
+		}
+		else {
+			node = best_child(node->child);
+		}
+	}
+	
+	// 자식노드를 다 만들면 방문하지 않은 자식노드를 찾아서 반환한다.
+	return pick_unvisited(node->child);
+}
+
+// root가 들어오면 Tree를 구성하도록 함
+Node* expand(Node *node, int turn) {
+	for (int y = node->getY() - 1; y <= node->getY() + 1; y++) {
+		for (int x = node->getX() - 1; x <= node->getX() + 1; x++) {
+			// 확장 중에서 중복 방지
+			bool overlap = false;
+			for (Node *p : node->child) {
+				if (x == p->getX() && y == p->getY()) overlap = true;
+			}
+			if (overlap) continue;
+
+			// 중복 아닌 자식 노드는 추가됨
+			int pos = showBoard(x, y);
+			Node *child = new Node(x, y);
+
+			node->setChild(child);
+			child->setParent(node);
+
+			if (pos == 0) {
+				if (turn == 1) {
+					child->setUnit(1);
+					virtualBoard[x][y] = 1;
+				}
+				else {
+					child->setUnit(2);
+					virtualBoard[x][y] = 2;
+				}
+			}
+			else if (pos == 1) {
+				child->setUnit(1);
+			}
+			else if (pos == 2) {
+				child->setUnit(2);
+			}
+			else {
+				if (turn == 1) {
+					child->setUnit(1);
+				}
+				else {
+					child->setUnit(2);
+				}
+			}
+		}
+	}
+	return node;
+}
+
+Node* pick_unvisited(vector<Node*> child) {
+	// 방문하지 않은 자식 노드 찾기
+	for (Node *p : child) {
+		if (p->getCount() == 0) return p;
+	}
+}
+
+bool fully_expanded(Node* node) {
+	// 확장 최대크기 지정
+	if (node->child.size() == 8) return true;
+	else return false;
+}
+
+bool non_terminal(Node *node) {
+	return is_gameEnd(node);
+}
+
+int simulation(Node *node) {
+	while (non_terminal(node)) {
+		node = policy(node);
+	}
+
+	return result(node);
+}
+
+Node* pick_random(vector<Node*> child) {
+	// 랜덤 시뮬을 위해
+	srand((unsigned)time(NULL));
+	return child[rand() % child.size()];
+}
+
+// 가능하면 유망한 것을 찾아야 할듯
+Node* policy(Node *node) {
+	return pick_random(node->child);
+}
+
+void backpropagate(Node* node, int result) {
+	// 마지막에 게임이 끝난 부분에서 root 까지 올라가면서 result를 부여해준다.
+	if (node->getParent() == nullptr) return;
+	node->update(result);
+	backpropagate(node->getParent(), result);
+}
+
+Node* best_child(vector<Node*> child, double cnode_parameter) {
+	double maxVal = 0.0;
+
+	// weight가 가장 높은 자식노드를 선택
+	for (Node *c : child) {
+		c->setWeight((c->getWin() / c->getCount()) + cnode_parameter * sqrt((2 * log(c->getCount())) / c->getCount()));
+
+		if (maxVal < c->getWeight()) {
+			maxVal = c->getWeight();
 		}
 	}
 
-	return true;
+	for (Node *c : child) {
+		if (maxVal == c->getWeight()) {
+			return c;
+		}
+	}
 }
 
-// 선형성 검사 함수
-bool lineCheck(int bx, int by, int cx, int cy, int level) {
+bool is_gameEnd(Node* node) {
+	return result(node) == 1;
+}
+
+
+int result(Node* node) {
+
+	if (checkSix(node, 1, 1) || checkSix(node, -1, 1) || checkSix(node, 0, 1) || checkSix(node, 1, 0)) {
+		return 1;
+	}
+	return 0;
+}
+
+bool checkSix(Node* node, int incX, int incY) {
+	int pos = node->getUnit();;
+	int x = node->getX();
+	int y = node->getY();
+	int six = 1;
+	
+	while (true) {
+		if (showBoard(x + incX, y + incY) == pos) six++;
+		else if (showBoard(x - incX, y - incY) == pos) six++;
+		
+		if (six > 6) return false;
+		else {
+			if (six == 6) return true;
+		}
+	}
+}
+
+
+// sub function
+bool empty(int pos) {
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			if (showBoard(i, j) == pos) return false;
+			return true;
+		}
+	}
+}
+
+bool line_check(int bx, int by, int cx, int cy, int level) {
 	bool check = false;
 
 	// level이 2 이하면 돌을 2개째 검사하는 것을 의미
@@ -401,13 +319,4 @@ bool lineCheck(int bx, int by, int cx, int cy, int level) {
 	return check;
 }
 
-// 적돌의 반대편 위치 가중치 설정
-void set_opposition(int x, int y) {
-	for (int j = center_y - 1; j <= center_y + 1; j++) {
-		for (int i = center_x - 1; i <= center_x + 1; i++) {
-			if (lineCheck(center_x, center_y, i, j, 3) && showBoard(i, j) == 0) {
-				val[j][i] -= 50;
-			}
-		}
-	}
-}
+
--- Connect6Algo.h
+++ Connect6Algo.h
@@ -3,7 +3,6 @@
 // <--------------- 이 Code를 수정하면  작동하지 않을 수 있습니다 ------------------>
 
 #pragma once
-
 extern int width, height;
 extern int terminateAI;
 extern unsigned s_time;
@@ -25,9 +24,7 @@ extern void writeLog(char *log);
 void myturn(int cnt = 2);
 char info[];
 
-// 추가한 것
-#include <vector>
-
+// Add 
 class Position {
 private:
 	int x;
@@ -40,29 +37,114 @@ public:
 	}
 	int getX() { return this->x; }
 	int getY() { return this->y; }
-
-	Position &operator =(const Position &Other) {
-		this->x = Other.x;
-		this->y = Other.y;
-	}
+	~Position() {}
 };
 
+#include <vector>
 extern std::vector<Position> myLog;
 extern std::vector<Position> enemyLog;
 
+// 한 좌표가 가지는 노드
+class Node {
+private:
+	int x, y;
+	int count = 0, win = 0;
+	int unit;
+	double weight = 0.0;
+	Node *parent = nullptr;
+public:
+	std::vector<Node *> child;
 
-void setMyWeight();
-void searchMyTree(int x, int y, int level);
-void setEnemyWeight();
-void searchEnemyTree(int x, int y, int level);
+	int getX() {
+		return this->x;
+	}
+	int getY() {
+		return this->y;
+	}
+	int getCount() {
+		return this->count;
+	}
+	int getWin() {
+		return this->win;
+	}
+	int getWeight() {
+		return this->weight;
+	}
+	int getUnit() {
+		return this->unit;
+	}
+	Node* getParent() {
+		return this->parent;
+	}
+	void setX(int x) {
+		this->x = x;
+	}
+	void setY(int y) {
+		this->y = y;
+	}
+	void setWeight(double weight) {
+		this->weight = weight;
+	}
+	void setChild(Node *n) {
+		child.push_back(n);
+	}
+	void setParent(Node *p) {
+		parent = p;
+	}
+	void setUnit(int unit) {
+		this->unit = unit;
+	}
+	void addCount(int count) {
+		this->count += count;
+	}
+	// result는 시뮬 결과 이기면 1, 지면 0이 들어온다.
+	void update(int result) {
+		this->count += 1;
+		this->win += result;
+	}
+	Node() {}
+	Node(int x, int y, int count = 0) {
+		this->x = x;
+		this->y = y;
+		this->count = count;
+	}
+	Node(int x, int y, int count, int win) {
+		this->x = x;
+		this->y = y;
+		this->count = count;
+		this->win = win;
+	}
+	~Node() {}
+	void operator =(Node *other) {
+		this->x = (*other).x;
+		this->y = (*other).y;
+		this->count = (*other).count;
+		this->win = (*other).win;
+	}
+	bool operator ==(Node &other) {
+		if (this->x == other.getX() && this->y == other.getY()) return true;
+		else return false;
+	}
+};
 
-bool empty();
-bool lineCheck(int bx, int by, int cx, int cy, int level);
-void set_opposition(int x, int y);
+// MCTS
+Node* yukmok();
+Node* MCTS(Node* root);
+Node* selection(Node* node, int turn);
+Node* expand(Node *node, int turn);
+Node* pick_unvisited(std::vector<Node*> child);
+bool fully_expanded(Node* node);
+bool non_terminal(Node *node);
+int simulation(Node *node);
+Node* pick_random(std::vector<Node*> child);
+Node* policy(Node *node);
+void backpropagate(Node* node, int result);
+Node* best_child(std::vector<Node*> child, double cnode_parameter = 1.4);
+bool is_gameEnd(Node* node);
+int result(Node* node);
+bool checkSix(Node* node, int incX, int incY);
 
-// ver0809
-void check();
-void checkMyWin();
-void checkEnemyWin();
-void checkMyPromising(int curX, int curY, int level);
-void checkEnemyPromising(int curX, int curY, int level);
+
+// Sub Function
+bool empty(int pos);
+bool line_check(int bx, int by, int cx, int cy, int level);
